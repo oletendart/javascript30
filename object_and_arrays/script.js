da5ba3f914940be644e728d6b39445a142ee8d8e
Olivia Letendart <letendartolivia@gmail.com>
2020-01-28 10:35:08 +0100

    commented out variables names + create new const contains arrays + callback const players in the const team + console.log

--- object_and_arrays/script.js
+++ object_and_arrays/script.js
@@ -1,14 +1,23 @@
 "use strict";
 
 // start with strings, numbers and booleans
-let age = 100;
-let age2 = age;
-console.log(age, age2);
-age = 200;
-console.log(age, age2);
 
-let name = "Wes";
-let name2 = name;
-console.log(name, name2);
-name = "wesley";
-console.log(name, name2);
+// let age = 100;
+// let age2 = age;
+// console.log(age, age2);
+// age = 200;
+// console.log(age, age2);
+
+// let name = "Wes";
+// let name2 = name;
+// console.log(name, name2);
+// name = "wesley";
+// console.log(name, name2);
+
+// Let's say we have an array
+const players = ["Wes", "Sarah", "Ryan", "Poppy"];
+
+// and we want to make a copy of it.
+const team = players;
+
+console.log(team);
