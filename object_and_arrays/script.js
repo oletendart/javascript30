3ab2689ffbd3f3b751df03920be6746ec016c563
Olivia Letendart <letendartolivia@gmail.com>
2020-01-28 10:30:57 +0100

    create variables name + console.log + modification variables name

--- object_and_arrays/script.js
+++ object_and_arrays/script.js
@@ -6,3 +6,9 @@ let age2 = age;
 console.log(age, age2);
 age = 200;
 console.log(age, age2);
+
+let name = "Wes";
+let name2 = name;
+console.log(name, name2);
+name = "wesley";
+console.log(name, name2);
