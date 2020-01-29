35b69c4c963a0fe24c7209fd2a4be7453859dc22
Olivia Letendart <letendartolivia@gmail.com>
2020-01-29 17:17:14 +0100

    create two const in the function add item, create object item, + create text

--- localstorage_eventdeleg/script.js
+++ localstorage_eventdeleg/script.js
@@ -6,6 +6,11 @@ const items = [];
 
 function addItem(e) {
   e.preventDefault();
+  const text = this.querySelector("[name=item]");
+  const item = {
+    text: "Item Name",
+    done: false
+  };
 }
 
 addItems.addEventListener("submit", addItem);
