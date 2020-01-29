d7c049ec8d9125afd119d01222e2d1a9346f77e3
Olivia Letendart <letendartolivia@gmail.com>
2020-01-29 17:12:52 +0100

    create function additem + create event listener

--- localstorage_eventdeleg/script.js
+++ localstorage_eventdeleg/script.js
@@ -3,3 +3,9 @@
 const addItems = document.querySelector(".add-items");
 const itemsList = document.querySelector(".plates");
 const items = [];
+
+function addItem(e) {
+  e.preventDefault();
+}
+
+addItems.addEventListener("submit", addItem);
