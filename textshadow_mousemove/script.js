71655a9029f439e7bad093b11a33e30eacc20799
Olivia Letendart <letendartolivia@gmail.com>
2020-01-31 14:37:49 +0100

    create two const + one const contains object

--- textshadow_mousemove/script.js
+++ textshadow_mousemove/script.js
@@ -3,6 +3,11 @@
 const hero = document.querySelector(".hero");
 const text = hero.querySelector("h1");
 
-function shadow(e) {}
+function shadow(e) {
+  const width = hero.offsetWidth;
+  const height = hero.offsetHeight;
+
+  const { offsetWidth: width, offsetHeight: height } = hero;
+}
 
 hero.addEventListener("mousemove", shadow);
