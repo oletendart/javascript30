78dc07be127c29e59c35cd6e50a27004f24f633d
Olivia Letendart <letendartolivia@gmail.com>
2020-01-31 14:51:26 +0100

    create const walk + create two const in the function shadow

--- textshadow_mousemove/script.js
+++ textshadow_mousemove/script.js
@@ -2,6 +2,7 @@
 
 const hero = document.querySelector(".hero");
 const text = hero.querySelector("h1");
+const walk = 100; // 100px
 
 function shadow(e) {
   const { offsetWidth: width, offsetHeight: height } = hero;
@@ -11,6 +12,9 @@ function shadow(e) {
     x = x + e.target.offsetLeft;
     y = y + e.target.offsetTop;
   }
+
+  const xWalk = (x / width) * walk - walk / 2;
+  const yWalk = (y / width) * walk - walk / 2;
 }
 
 hero.addEventListener("mousemove", shadow);
