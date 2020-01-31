a78eda66b939965cdd7c769748a1675822b1b2a4
Olivia Letendart <letendartolivia@gmail.com>
2020-01-31 14:49:27 +0100

    create if in the function shadow for the x and y

--- textshadow_mousemove/script.js
+++ textshadow_mousemove/script.js
@@ -6,6 +6,11 @@ const text = hero.querySelector("h1");
 function shadow(e) {
   const { offsetWidth: width, offsetHeight: height } = hero;
   let { offsetX: x, offsetY: y } = e;
+
+  if (this !== e.target) {
+    x = x + e.target.offsetLeft;
+    y = y + e.target.offsetTop;
+  }
 }
 
 hero.addEventListener("mousemove", shadow);
