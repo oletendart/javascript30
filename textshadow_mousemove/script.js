e04d635052df48133762e761877b3f5f34be9d94
Olivia Letendart <letendartolivia@gmail.com>
2020-01-31 14:44:55 +0100

    modificaton function + create variable

--- textshadow_mousemove/script.js
+++ textshadow_mousemove/script.js
@@ -4,10 +4,8 @@ const hero = document.querySelector(".hero");
 const text = hero.querySelector("h1");
 
 function shadow(e) {
-  const width = hero.offsetWidth;
-  const height = hero.offsetHeight;
-
   const { offsetWidth: width, offsetHeight: height } = hero;
+  let { offsetX: x, offsetY: y } = e;
 }
 
 hero.addEventListener("mousemove", shadow);
