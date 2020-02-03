37e9bbb345b5a5ccda05098ac2eb262b3f141383
Olivia Letendart <letendartolivia@gmail.com>
2020-02-03 10:39:21 +0100

    modification const sortedBands, function arrow + queryselector class bands + insert html

--- sorting_band_names/script.js
+++ sorting_band_names/script.js
@@ -20,8 +20,10 @@ function strip(bandName) {
   return bandName.replace(/^(a |the |an )/i, "").trim();
 }
 
-const sortedBands = bands.sort(function(a, b) {
-  return strip(a) > strip(b) ? 1 : -1;
-});
+const sortedBands = bands.sort((a, b) => (strip(a) > strip(b) ? 1 : -1));
+
+document.querySelector(".bands").innerHTML = sortedBands
+  .map(band => `<li>${band}</li>`)
+  .join("");
 
 console.log(sortedBands);
