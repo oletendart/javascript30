51aa37501bdf798cfe6c051f47de4f15c11f379e
Olivia Letendart <letendartolivia@gmail.com>
2020-02-03 11:11:58 +0100

    create const mins + callback secondsLeft

--- adding_up_times/script.js
+++ adding_up_times/script.js
@@ -13,3 +13,6 @@ const seconds = timeNodes
 let secondsLeft = seconds;
 const hours = Math.floor(secondsLeft / 3600);
 secondsLeft = secondsLeft % 3600;
+
+const mins = Math.floor(secondsLeft / 60);
+secondsLeft = secondsLeft % 60;
