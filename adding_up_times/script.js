8880d7562c453ec5d3c2b454c304b3a2e27ceac5
Olivia Letendart <letendartolivia@gmail.com>
2020-02-03 10:59:41 +0100

    modification const seconds + create new const in the seconds

--- adding_up_times/script.js
+++ adding_up_times/script.js
@@ -2,4 +2,10 @@
 
 const timeNodes = Array.from(document.querySelector("[data-time]"));
 
-const seconds = timeNodes.map(node => node.dataset.time);
+const seconds = timeNodes
+  .map(node => node.dataset.time)
+  .map(timeCode => {
+    const [mins, secs] = timeCode.split(":");
+    return mins * 60 + secs;
+    console.log(mins, secs);
+  });
