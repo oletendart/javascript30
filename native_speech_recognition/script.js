918284151032b9165e9c893773768ff9d0586161
Olivia Letendart <letendartolivia@gmail.com>
2020-02-04 15:35:20 +0100

    modification event listener recognition, create const transcript

--- native_speech_recognition/script.js
+++ native_speech_recognition/script.js
@@ -13,7 +13,11 @@ const words = document.querySelector(".words");
 words.appendChild(p);
 
 recognition.addEventListener("results", e => {
-  console.log(e);
+  console.log(e.results);
+  const transcript = Array.from(e.results)
+    .map(result => result[0])
+    .map(result => result.transcript);
+  console.log(transcript);
 });
 
 recognition.start();
