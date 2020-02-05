42b3fc773a36a8f72c4ae3cf328195c6a0801ce4
Olivia Letendart <letendartolivia@gmail.com>
2020-02-05 09:57:26 +0100

    create geolocation position + create console.log

--- geolocation/script.js
+++ geolocation/script.js
@@ -2,3 +2,8 @@
 
 const arrow = document.querySelector(".arrow");
 const speed = document.querySelector(".speed-value");
+
+navigator.geolocation.watchPosition(data => {
+  console.log(data);
+  speed.textContent = data.coords.speed;
+});
