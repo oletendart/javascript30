b17761bbedc1beb7f55902a7d4e304884d29d810
Olivia Letendart <letendartolivia@gmail.com>
2020-02-06 16:07:28 +0100

    create new style transform for the highlight coord

--- follow_along_link/script.js
+++ follow_along_link/script.js
@@ -11,6 +11,7 @@ function highlightLink() {
   console.log(linkCoords);
   highlight.style.width = `${linkCoords.width}px`;
   highlight.style.height = `${linkCoords.height}px`;
+  highlight.style.transform = `translate(${linkCoords.left}px, ${linkCoords.top}px)`;
 }
 
 triggers.forEach(a => a.addEventListener("mouseenter", highlightLink));
