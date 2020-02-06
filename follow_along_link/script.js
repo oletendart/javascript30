702d0ca577890cfd4aa105bab4c0d3602084da65
Olivia Letendart <letendartolivia@gmail.com>
2020-02-06 16:10:38 +0100

    create const coords contains object, key and value : width, height, top and left

--- follow_along_link/script.js
+++ follow_along_link/script.js
@@ -9,6 +9,12 @@ document.body.append(highlight);
 function highlightLink() {
   const linkCoords = this.getBoudingClientRect();
   console.log(linkCoords);
+  const coords = {
+    width: linkCoords.width,
+    height: linkCoords.height,
+    top: linkCoords.top - window.scrollY,
+    left: linkCoords.left - window.scrollX
+  };
   highlight.style.width = `${linkCoords.width}px`;
   highlight.style.height = `${linkCoords.height}px`;
   highlight.style.transform = `translate(${linkCoords.left}px, ${linkCoords.top}px)`;
