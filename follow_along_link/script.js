b84b489f81c28513eb045c1db7cd2f2fd255d9f2
Olivia Letendart <letendartolivia@gmail.com>
2020-02-06 15:43:10 +0100

    create const hightlight, create in the document the tag span + list in the document

--- follow_along_link/script.js
+++ follow_along_link/script.js
@@ -1,3 +1,7 @@
 "use strict";
 
 const triggers = document.querySelector("a");
+const highlight = document.createElement("span");
+
+highlight.classList.add("highlight");
+document.body.append(highlight);
