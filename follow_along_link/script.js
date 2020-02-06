42e51f87f6b379eb18cdfd0c96b7f41d83d434ce
Olivia Letendart <letendartolivia@gmail.com>
2020-02-06 15:56:07 +0100

    create function highlight link + create for each contains event listener

--- follow_along_link/script.js
+++ follow_along_link/script.js
@@ -5,3 +5,9 @@ const highlight = document.createElement("span");
 
 highlight.classList.add("highlight");
 document.body.append(highlight);
+
+function highlightLink() {
+  console.log("Highlight !!!");
+}
+
+triggers.forEach(a => a.addEventListener("mouseenter", highlightLink));
