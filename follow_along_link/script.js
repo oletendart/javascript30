36f23ab024f4e95b421b1bb3e3133b194d368d30
Olivia Letendart <letendartolivia@gmail.com>
2020-02-06 16:29:51 +0100

    modificate const coords + finish excercices

--- follow_along_link/script.js
+++ follow_along_link/script.js
@@ -1,20 +1,20 @@
 "use strict";
 
-const triggers = document.querySelector("a");
+const triggers = document.querySelectorAll("a");
 const highlight = document.createElement("span");
-
 highlight.classList.add("highlight");
-document.body.append(highlight);
+document.body.appendChild(highlight);
 
 function highlightLink() {
-  const linkCoords = this.getBoudingClientRect();
+  const linkCoords = this.getBoundingClientRect();
   console.log(linkCoords);
   const coords = {
     width: linkCoords.width,
     height: linkCoords.height,
-    top: linkCoords.top - window.scrollY,
-    left: linkCoords.left - window.scrollX
+    top: linkCoords.top + window.scrollY,
+    left: linkCoords.left + window.scrollX
   };
+
   highlight.style.width = `${coords.width}px`;
   highlight.style.height = `${coords.height}px`;
   highlight.style.transform = `translate(${coords.left}px, ${coords.top}px)`;
