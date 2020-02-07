e4e134d3155e56ca52bdfca6b795fd02ce607e00
Olivia Letendart <letendartolivia@gmail.com>
2020-02-07 10:54:02 +0100

    modificate populate voice + finish exercice

--- speech_synthesis/script.js
+++ speech_synthesis/script.js
@@ -11,6 +11,7 @@ msg.text = document.querySelector('[name="text"]').value;
 function populateVoices() {
   voices = this.getVoices();
   voicesDropdown.innerHTML = voices
+    .filter(voice => voice.lang.includes("en"))
     .map(
       voice =>
         `<option value="${voice.name}">${voice.name} (${voice.lang})</option>`
@@ -40,4 +41,4 @@ speechSynthesis.addEventListener("voiceschanged", populateVoices);
 voicesDropdown.addEventListener("change", setVoice);
 options.forEach(option => option.addEventListener("change", setOption));
 speakButton.addEventListener("click", toggle);
-stopButton.addEventListener("click", toggle(false));
+stopButton.addEventListener("click", () => toggle(false));
