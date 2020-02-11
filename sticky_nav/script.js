9e266026945044280401ceb25483bd74af0358a5
Olivia Letendart <letendartolivia@gmail.com>
2020-02-11 15:25:27 +0100

    create new const topOfNav + modification function fixnav

--- sticky_nav/script.js
+++ sticky_nav/script.js
@@ -1,8 +1,9 @@
 "use strict";
 
 const nav = document.querySelector(".main");
+const topOfNav = nav.offsetTop;
 
 function fixNav(e) {
-  console.log(e);
+  console.log(topOfNav);
 }
 window.addEventListener("scroll", fixNav);
