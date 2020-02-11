8134af1bdaa306b7c3419932dfdd55f1587ac01e
Olivia Letendart <letendartolivia@gmail.com>
2020-02-11 15:27:54 +0100

    modification function fixNav, create loop if in the function

--- sticky_nav/script.js
+++ sticky_nav/script.js
@@ -4,6 +4,10 @@ const nav = document.querySelector(".main");
 const topOfNav = nav.offsetTop;
 
 function fixNav(e) {
-  console.log(topOfNav);
+  if (window.scrollY >= topOfNav) {
+    document.body.classList.add("fixed-nav");
+  } else {
+    document.body.classList.remove("fixed-nav");
+  }
 }
 window.addEventListener("scroll", fixNav);
