aae6a649283210409dbb5c3916709c66d359d463
Olivia Letendart <letendartolivia@gmail.com>
2020-02-12 11:19:19 +0100

    create const targetting div in the index.html + create function logtext contains console.log + create event listener

---
+++ event_capt-pro-bub-once/script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const divs = document.querySelectorAll("div");
+
+function logText(e) {
+  console.log(this.classList.value);
+}
+
+divs.forEach(div => div.addEventListener("click", logText));
