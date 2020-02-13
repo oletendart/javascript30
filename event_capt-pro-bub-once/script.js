1ab53d6a64ff89720d7a8b0ced3c09b0473d8482
Olivia Letendart <letendartolivia@gmail.com>
2020-02-13 10:38:47 +0100

    stop propagation in the comment + create once in the event click

--- event_capt-pro-bub-once/script.js
+++ event_capt-pro-bub-once/script.js
@@ -4,13 +4,14 @@ const divs = document.querySelectorAll("div");
 
 function logText(e) {
   console.log(this.classList.value);
-  e.stopPropagation();
+  /* e.stopPropagation(); // Stop bublling ! */
 }
 
 document.body.addEventListener("click", logText);
 
 divs.forEach(div =>
   div.addEventListener("click", logText, {
-    capture: false
+    capture: false,
+    once: true
   })
 );
