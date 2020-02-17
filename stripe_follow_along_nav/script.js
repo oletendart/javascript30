044aa56aa402651468904865b5a5d9412d65cb62
Olivia Letendart <letendartolivia@gmail.com>
2020-02-17 11:08:00 +0100

    create setTimeout function in the function handleEnter targetting and create trigger-enter

--- stripe_follow_along_nav/script.js
+++ stripe_follow_along_nav/script.js
@@ -5,7 +5,10 @@ const background = document.querySelector(".dropdownBackground");
 const nav = document.querySelector(".top");
 
 function handleEnter() {
-  console.log("Enter");
+  this.classLit.add("trigger-enter");
+  setTimeout(() => {
+    this.classList.add("trigger-enter-active");
+  }, 150);
 }
 
 function handleLeave() {
