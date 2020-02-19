9dc9945dcfe5923cf447284a1ec770997a0f733a
Olivia Letendart <letendartolivia@gmail.com>
2020-02-19 11:40:30 +0100

    create const slider tarrgeting class items + create variable isDown, startX and scroll Left+ create four event listener mouse

---
+++ click_and_drag/script.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const slider = document.querySelector(".items");
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener("mousedown", () => {});
+slider.addEventListener("mouseleave", () => {});
+slider.addEventListener("mouseup", () => {});
+slider.addEventListener("mousemove", () => {});
