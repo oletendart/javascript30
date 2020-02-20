0702310cff58d0c885e302bca9ddde04f2c6c03e
Olivia Letendart <letendartolivia@gmail.com>
2020-02-20 10:08:04 +0100

    create console count in the event listener mousemove + create classlist add and remove in the different event listener in the files script.js

--- click_and_drag/script.js
+++ click_and_drag/script.js
@@ -7,14 +7,17 @@ let scrollLeft;
 
 slider.addEventListener("mousedown", () => {
   isDown = true;
+  slider.classList.add("active");
 });
 slider.addEventListener("mouseleave", () => {
   isDown = false;
+  slider.classList.remove("active");
 });
 slider.addEventListener("mouseup", () => {
   isDown = false;
+  slider.classList.remove("active");
 });
 slider.addEventListener("mousemove", () => {
-  console.log(isDown);
-  console.log("Do Work");
+  if (!isDown) return; // stop the fn from running
+  console.count(isDown);
 });
