39ba80d998217ba11c3914fe555d981082a9c8fa
Olivia Letendart <letendartolivia@gmail.com>
2020-02-20 10:18:47 +0100

    definite variable scrollLeft + create prevent default in the event mousemove + create const x in the function

--- click_and_drag/script.js
+++ click_and_drag/script.js
@@ -9,6 +9,7 @@ slider.addEventListener("mousedown", e => {
   isDown = true;
   slider.classList.add("active");
   startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
 });
 slider.addEventListener("mouseleave", () => {
   isDown = false;
@@ -18,8 +19,8 @@ slider.addEventListener("mouseup", () => {
   isDown = false;
   slider.classList.remove("active");
 });
-slider.addEventListener("mousemove", () => {
+slider.addEventListener("mousemove", e => {
   if (!isDown) return; // stop the fn from running
-  console.count(isDown);
-  console.log(startX);
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
 });
