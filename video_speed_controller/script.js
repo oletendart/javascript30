e32adea61f2c46cd64b9353bff30054a5a54a11b
Olivia Letendart <letendartolivia@gmail.com>
2020-02-21 11:55:05 +0100

    create const percent in the event listener

--- video_speed_controller/script.js
+++ video_speed_controller/script.js
@@ -6,5 +6,7 @@ const video = document.querySelector(".flex");
 
 speed.addEventListener("mousemove", function(e) {
   const y = e.pageY - this.offsetTop;
+  const percent = y / this.offsetHeight;
   console.log(y);
+  console.log(percent);
 });
