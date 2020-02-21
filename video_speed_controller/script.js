172007ac0d59de85dd9fac4f64b9585f319630ab
Olivia Letendart <letendartolivia@gmail.com>
2020-02-21 11:31:25 +0100

    create three const, speed, bar and video + create event listener

---
+++ video_speed_controller/script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const speed = document.querySelector(".speed");
+const bar = speed.querySelector(".speed-bar");
+const video = document.querySelector(".flex");
+
+speed.addEventListener("mousemove", function(e) {
+  console.log(e);
+});
